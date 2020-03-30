0fcc1d52de29548f0497ecdc4c0038faea7e3390
mayur-bhandari <mayur.bhandari@inscripts.com>
2020-03-30 11:15:20 +0530

    first commit

---
+++ test/pass.test.js
@@ -0,0 +1,35 @@
+const window = require("browser-env")({
+  url: "http://www.runtestcases.com",
+  contentType: "text/html",
+  includeNodeLocations: true,
+  storageQuota: 100000000000000
+});
+
+const fetch = require("node-fetch");
+window.fetch = fetch;
+global.fetch = fetch;
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const tests = [0,1,2,3,4,5];
+
+describe("Will pass the test", function() {
+  this.timeout(0);
+  tests.map(test => {
+    it(test+" test", function() {
+      if(test === 4){
+        expect(true).to.be.false;
+      }else{
+        expect(true).to.be.true;
+      }
+    });
+  })
+});
+
+// describe("Will fail the test", function() {
+//   this.timeout(0);
+//   it("second test", function() {
+//     expect(true).to.be.false;
+//   });
+// });
